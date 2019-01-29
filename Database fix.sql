4d3453c3f64a50f920f26efb9d1c84c2ad811098
yogafdl <fadillah2508@gmail.com>
2019-01-29 20:15:39 +0700

    update ke dua

--- Database fix.sql
+++ Database fix.sql
@@ -26,3 +26,45 @@ CREATE TABLE Laporan(
 	FOREIGN KEY(ID_Adm) REFERENCES Admin(ID_Adm) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
+INSERT INTO Laporan VALUES
+('4001','5001',2019-01-01,500000,300000),
+('4002','5001',2019-01-03,600000,500000)
+
+
+--
+-- Tabel 'Kios'
+
+--
+
+CREATE TABLE Kios(
+	No_Kios varchar(7),
+    ID_Adm varchar(7),
+    No_Laporan varchar(7),
+	Nama_Pemilik varchar(50),
+	Nama_Kios varchar(50),
+	No_Tlp int(12),
+	PRIMARY KEY(No_Kios),
+	FOREIGN KEY(ID_Adm) REFERENCES Admin(ID_Adm) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY(No_Laporan) REFERENCES Laporan(No_Laporan) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+INSERT INTO Kios VALUES
+('2001','5002','4001','Suparman','Ayam Ayaman',081264759870),
+('2002','5002','4002','Supriman','Daging Dagingan',089789652453)
+
+--
+-- Tabel 'Makanan'
+--
+
+CREATE TABLE Makanan(
+	ID_Makanan varchar(7),
+	Nama_Makanan varchar(50),
+	Harga varchar(50),
+	
+	PRIMARY KEY(ID_Makanan)
+);
+
+INSERT INTO Makanan VALUES
+('3001','Ayam Goreng','10000'),
+('3002','Sapi Goreng','15000')
+

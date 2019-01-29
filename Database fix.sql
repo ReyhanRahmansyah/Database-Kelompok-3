495142334a842cfed4e636d6ca65f2473bc8f4ee
prasetyaheryantos <prasetya.hshadiq@gmail.com>
2019-01-29 20:14:35 +0700

    Update Database fix.sql

--- Database fix.sql
+++ Database fix.sql
@@ -26,3 +26,39 @@ CREATE TABLE Laporan(
 	FOREIGN KEY(ID_Adm) REFERENCES Admin(ID_Adm) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
+--
+-- Tabel 'Kios'
+
+--
+
+CREATE TABLE Kios(
+	No_Kios varchar(7),
+    ID_Adm varchar(7),
+    No_Laporan varchar(7),
+	Nama_Pemilik varchar(50),
+	Nama_Kios varchar(50),
+	No_Tlp int(12),
+	PRIMARY KEY(No_Kios),
+	FOREIGN KEY(ID_Adm) REFERENCES Admin(ID_Adm) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY(No_Laporan) REFERENCES Laporan(No_Laporan) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+INSERT INTO Kios VALUES
+('2001','5002','4001','Suparman','Ayam Ayaman',081264759870),
+('2002','5002','4002','Supriman','Daging Dagingan',089789652453)
+
+--
+-- Tabel 'Makanan'
+--
+
+CREATE TABLE Makanan(
+	ID_Makanan varchar(7),
+	Nama_Makanan varchar(50),
+	Harga varchar(50),
+	
+	PRIMARY KEY(ID_Makanan)
+);
+
+INSERT INTO Makanan VALUES
+('3001','Ayam Goreng','10000'),
+('3002','Sapi Goreng','15000')
